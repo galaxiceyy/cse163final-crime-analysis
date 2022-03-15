89708be9541462bdacd3fb80fffdc6aae1f5842b
Alec Gao <62917571+Alecake2@users.noreply.github.com>
2022-03-15 02:15:44 -0700

    Flake 8

--- data_cleaning.py
+++ data_cleaning.py
@@ -7,8 +7,13 @@ def preprocess_crime_data(SPD_file_name):
     filter the crime incidents occurred between 2018-2022.
     add zip code column from long/lat coordinates
     """
-    chunk = pd.read_csv(SPD_file_name, chunksize=1000, usecols=["Report Number", "Offense Start DateTime", "Report DateTime", "Offense Parent Group",
-        "Offense Code", "Offense", "MCPP", "Longitude", "Latitude"])
+    chunk = pd.read_csv(SPD_file_name, chunksize=1000,
+                        usecols=["Report Number",
+                                 "Offense Start DateTime",
+                                 "Report DateTime",
+                                 "Offense Parent Group",
+                                 "Offense Code", "Offense",
+                                 "MCPP", "Longitude", "Latitude"])
     df = pd.concat(chunk)
     df['date2'] = pd.to_datetime(df['Offense Start DateTime'])
     df['Year'] = df['date2'].dt.year
@@ -16,13 +21,15 @@ def preprocess_crime_data(SPD_file_name):
     df['Day'] = df['date2'].dt.day
     df['Hour'] = df['date2'].dt.hour
     df['Minute'] = df['date2'].dt.minute
-    df = df.drop(['Offense Start DateTime'], axis=1) 
-    df = df.drop(['date2'], axis=1) 
+    df = df.drop(['Offense Start DateTime'], axis=1)
+    df = df.drop(['date2'], axis=1)
     df = df[df['Year'] >= 2016]
     df.to_csv('cleaned_crime_data.csv', index=False)
 
+
 def main():
-   preprocess_crime_data('SPD_Crime_Data.csv')
+    preprocess_crime_data('SPD_Crime_Data.csv')
+
 
 if __name__ == "__main__":
     main()
